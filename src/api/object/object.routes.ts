f33b4ad19e5c01ca23fcf5accbe014b2296a7b1d
DimRev <dimarev444@gmail.com>
2024-02-05 21:55:24 +0200

    Add api route handler for invalid routings

--- src/api/object/object.routes.ts
+++ src/api/object/object.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import objectController from './object.controller'
+import { loggerService } from '../../services/logger.service'
 
 export const objectRouter = express.Router()
 
@@ -8,3 +9,7 @@ objectRouter.get('/get/:objectId', objectController.readObject)
 objectRouter.get('/get/', objectController.readAllObjects)
 objectRouter.patch('/update/:objectId', objectController.updateObject)
 objectRouter.delete('/delete/:objectId', objectController.deleteObject)
+objectRouter.get('/**', (req, res, next) => {
+  loggerService.warn('Attempt to access invalid route')
+  res.status(400).send({ message: 'Invalid route' })
+})
